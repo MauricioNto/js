8353a70a72189f923535f38e4f290eb4348fe2db
MauricioNto <diegomauricionieto@gmail.com>
2015-07-19 00:47:08 -0500

    4
    4

--- jquery/killer.js
+++ jquery/killer.js
@@ -1,6 +1,15 @@
 $(document).ready(function () 
 { 
 
-	$('#hola').text("Soy texto");
+	$("#hola").click(
+
+		function() {
+
+		$('#hola').html('<iframe width="854" height="510" src="https://www.youtube.com/embed/weGLcc4HbkQ" frameborder="0" allowfullscreen></iframe>');
+		
+		}
+
+	);
+	
 
 });
